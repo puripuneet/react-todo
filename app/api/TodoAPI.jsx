bc8c8d07c968a3c3c7112de6540c04117d0bc24b
Puneet Puri <puneet@zikara.com>
2017-05-19 19:02:31 -0400

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,31 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // Filter by searchTest
+        filteredTodos = filteredTodos.filter((todo) => {
+            return (searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) > -1);
+        });
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if(!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
